cee1094a1f9653a8aebdc25138dd80e789accf5b
BoscoTU <chungchaktu@gmail.com>
2025-07-21 16:21:33 -0700

    works: determine voltage + current change

---
+++ single_stage_multimeter/single_stage_multimeter.ino
@@ -0,0 +1,29 @@
+int adcValue;
+float firstVoltage;
+double amp;
+
+unsigned long lastTelemetryTime = 0;
+long telemetryInterval = 200;
+
+float REFERENCE_VOLTAGE = 5.0;
+float CIRCUIT_RESISTANCE = 11;
+
+void setup() {
+  Serial.begin(9600);
+  analogReference(REFERENCE_VOLTAGE);
+}
+
+void loop() {
+  adcValue = analogRead(A0);
+  firstVoltage = adcValue * (REFERENCE_VOLTAGE / 1023.0);
+  
+  amp =  (firstVoltage)/ CIRCUIT_RESISTANCE;
+
+  if (millis() - lastTelemetryTime >= telemetryInterval) {
+    lastTelemetryTime = millis();
+    Serial.print(" v1: ");
+    Serial.print(firstVoltage);
+    Serial.print(" i: ");
+    Serial.println(amp);
+  }
+}
